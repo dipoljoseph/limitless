b7347bf042b67b18cd55dd028956fcfae13b6d68
dipoljoseph <97227464+dipoljoseph@users.noreply.github.com>
2023-06-17 17:12:04 +0800

    Add files via upload

---
+++ Limitless/limitless.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <title>Limitless</title>
+</head>
+<body>
+    <!--navigation/header-->
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo"><img src="limitless/layer 14.png" alt="logo" class="logo-navbar"></label>
+        <ul> 
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Blogs</a></li>
+            <li><a href="#">Our Partners</a></li>
+            <li><a href="#">Contact</a></li>
+            <li class="nav-no"><label><img src="limitless/phone icon.png" alt="phone icon" class="phone-icon">0917 124 4994</label</li>
+        </ul>
+    </nav>
+
+    <!--slider-->
+    <img src="limitless/slider.jpg" alt="header image" class="slider">
+    
+    <!--our services-->
+    <div class="services">
+        <img src="limitless/favicon_100px.png" alt="header image" class="service-logo">
+        <h1>OUR SERVICES</h1>
+
+        <div class="column">
+            <div class="col-left container">
+                <img src="limitless/03.jpg" alt="header image" class="service-pics">
+                <div class="centered-bot"><h3>Interior Designing</h3></div>
+            </div>
+            <div class="col-mid container">
+                <img src="limitless/02.jpg" alt="header image" class="service-pics">
+                <div class="centered-bot"><h3>Residential Projects</h3></div>
+            </div>
+            <div class="col-right container">
+                <img src="limitless/01.jpg" alt="header image" class="service-pics">
+                <div class="centered"><h3>Commercial Building</h3>
+                <br>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p></div>
+            </div>
+        </div>
+
+        <h2 class="textbox">BY PIANNE LIM DESIGNS</h2>
+        <img src="limitless/layer 13.png" alt="header image" class="line-break">
+    </div>
+
+    <!--our services-->
+    <div class="our-comp">
+        <div class="column">
+            <div class="comp-leftcol">
+                <img src="limitless/layer 3.png" alt="header image" class="comp-img">
+            </div>
+            <div class="comp-rightcol">
+                <h3>OUR COMPANY</h3>
+                <h1>WE DO OUR BEST IN BUILDING YOUR IDEAS</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                <br><br>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+
+                <button class="yellow-button">LEARN MORE</button>
+            </div>
+        </div>
+    </div>
+
+    <!--our portfolio-->
+    <div class="portfolio">
+        <p>PORTFOLIO</p>
+        <h1>OUR PROJECTS</h1>
+
+        <p>
+            <a href="#" style="color: #FFB74D;">ALL WORKS</a>
+            <a href="#">ARCHITECTURE</a>
+            <a href="#">RESIDENTIAL</a>
+            <a href="#">COMMERCIAL</a>
+            <a href="#">INTERIOR</a>
+        </p>
+
+        <div class="pic-row">
+            <img src="limitless/05.jpg" alt="header image" class="port-img">
+            <img src="limitless/06.jpg" alt="header image" class="port-img">
+            <img src="limitless/07.jpg" alt="header image" class="port-img">
+            <img src="limitless/08.jpg" alt="header image" class="port-img">
+        </div>
+        <div class="pic-row">
+            <img src="limitless/09.jpg" alt="header image" class="port-img">
+            <img src="limitless/010.jpg" alt="header image" class="port-img">
+            <img src="limitless/011.jpg" alt="header image" class="port-img">
+            <img src="limitless/012.jpg" alt="header image" class="port-img">
+        </div>
+    </div>
+    <div class="p-footer">
+        <div class="p-row">
+            <b>[125</b> 
+        </div>
+        <div class="p-row">
+            <pre>PROJECT</pre>
+            <pre>COMPLETED</pre>
+        </div>
+
+        <div class="p-row">
+            <b>[14</b>
+        </div>
+        <div class="p-row">
+            <pre>YEARS OF</pre>
+            <pre>EXPERIENCED</pre>
+        </div>
+
+        <div class="p-row">
+            <b>[184</b>
+        </div>
+        <div class="p-row">
+            <pre>HAPPY</pre>
+            <pre>CLIENTS</pre>
+        </div>
+
+        <div class="p-row">
+            <b>[56</b>
+        </div>
+        <div class="p-row">
+            <pre>BUSINESS</pre>
+            <pre>AWARDS</pre>
+        </div>
+    </div>
+
+    <!--team members-->
+    <div class="team-members">
+        <p>TEAM MEMBERS</p>
+        <h1>OUR ENGINEERS</h1>
+        <img src="limitless/team.png" alt="header image" class="team-png">
+    </div>
+
+    <img src="limitless/video.jpg" alt="header image" class="team-png">
+
+    <div class="team-members">
+        <p>HAPPY CLIENTS</p>
+        <h1>PARTNERS IN THE INDUSTRY</h1>
+        <p class="hc-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <img src="limitless/layer 8.jpg" alt="header image" class="teamlogo-png">
+        <img src="limitless/layer 9.jpg" alt="header image" class="teamlogo-png">
+        <img src="limitless/layer 10.jpg" alt="header image" class="teamlogo-png">
+        <img src="limitless/layer 11.jpg" alt="header image" class="teamlogo-png">
+    </div>
+
+    <!--footer-->
+    <div class="footer">
+        <div class="column">
+            <div class="f-leftcolumn">
+                <img src="limitless/1.png" alt="client review" class="f-logo">
+
+                <p class="f-namedesc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+            </div>
+
+            <div class="f-midcolumn">
+                <a href="#">Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Properties</a>
+                <a href="#">Blog</a>
+                <a href="#">Event</a>
+                <a href="#">Contact Us</a>
+            </div>
+            
+            <div class="f-rightcolumn">
+                <h2>SUBSCRIBE TO OUR NEWSLETTER </h2>
+
+                <input type="text">
+
+                <button class="yellow-button">SUBMIT</button>
+
+                <h2>FOLLOW US ON OUR SOCIAL MEDIA</h2>
+
+                    <img src="limitless/fb.png" alt="client review" class="c-logo">
+                    <img src="limitless/tw.png" alt="client review" class="c-logo">
+                    <img src="limitless/ig.png" alt="client review" class="c-logo">
+                    <img src="limitless/yt.png" alt="client review" class="c-logo">
+            </div>
+        </div>
+
+        <hr class="footer-line">
+
+        <p class="copyright">Copyright 2022 | All Rights Reserved | Developed by: ITworks</p>
+    </div>
+    
+</body>
+</html>
